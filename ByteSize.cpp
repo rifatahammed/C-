5230c609c069355da180dd55de788e283f26a4c7
Rifat Ahammed <reardj007@gmail.com>
2024-07-27 18:39:29 +0600

    check byte size

---
+++ ByteSize.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int yearOfBirth;
+    char gender;
+    bool isOlderThan18;
+    float cgpa;
+    double balance;
+
+    cout << "Size of is " << sizeof(int) << " bytes\n";
+    cout << "Int min value is " << INT_MIN <<endl;
+    cout << "Int max value is " << INT_MAX << endl;
+ 
+
+    system("pause>0");
+}
+
+
