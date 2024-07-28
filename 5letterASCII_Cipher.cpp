f170f3fe6628deed73c84afa72ecb370e04737f9
Rifat Ahammed <reardj007@gmail.com>
2024-07-28 22:33:13 +0600

    Ascii cipher added

---
+++ 5letterASCII_Cipher.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char c1, c2, c3, c4, c5, c6;
+    cout << "Enter 5 letters: ";
+    cin >> c1 >> c2 >> c3 >> c4 >> c5 >> c6;
+    cout << "ASCII message: " << int(c1) << int(c2) << int(c3) << int(c4) << int(c5)<<int(c6);
+    
+    system("pause>0");
+}
+
+
