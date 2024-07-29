e56f5e8a96795c7aca7e8b485ac2c9db1cefd7ae
Rifat Ahammed <reardj007@gmail.com>
2024-07-29 19:07:37 +0600

    Even Odd

---
+++ EvenOdd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int num;
+	cout << "Enter a whole number: ";
+	cin >> num;
+	if (num % 2 == 0) {
+		cout << "This is an even number" << endl;
+	}
+	else
+	{
+		cout << "This is an odd number" << endl;
+	}
+
+	system("pause>0");
+}
+
